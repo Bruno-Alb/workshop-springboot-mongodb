95d02f9fcc7eab6aa75ca7f9ecf806dba719de6b
Bruno Alberto <bruno.alb.fer@gmail.com>
2025-06-22 14:24:20 -0300

    Sexto commit - obtendo usuario por Id

--- src/main/java/com/bruno_alb/mongodb/services/UserService.java
+++ src/main/java/com/bruno_alb/mongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.bruno_alb.mongodb.services;
 
 import com.bruno_alb.mongodb.domain.User;
 import com.bruno_alb.mongodb.repository.UserRepository;
+import com.bruno_alb.mongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll (){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
